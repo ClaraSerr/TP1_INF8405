57e8a054fdd9f4fe6f6f0942eff4172d8fcbdec9
pedgarsan01 <117484200+pedgarsan01@users.noreply.github.com>
2023-01-23 11:54:53 -0500

    Version with return button in More Inf

--- app/src/main/java/com/example/tp1inf8405/More_Information.java
+++ app/src/main/java/com/example/tp1inf8405/More_Information.java
@@ -2,7 +2,9 @@ package com.example.tp1inf8405;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class More_Information extends AppCompatActivity {
 
@@ -10,6 +12,10 @@ public class More_Information extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more_information);
+    }
+    public void backmenu(View v){
+        Intent i= new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 
 }
