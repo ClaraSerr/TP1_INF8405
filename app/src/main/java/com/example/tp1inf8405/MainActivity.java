855d324f37b0435cb15406b6bcf2d23f3d74e627
eaboom <emileaboom@gmail.com>
2023-01-24 10:32:36 -0500

    Emile Studio 3

--- app/src/main/java/com/example/tp1inf8405/MainActivity.java
+++ app/src/main/java/com/example/tp1inf8405/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void moreinf(View v){
-        //test from Studio 2
+        //test from Studio 3
         Intent i= new Intent(this, More_Information.class);
         startActivity(i);
     }
