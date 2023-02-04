8090d966f112195201a0e6d112db2e7de69e64a7
ErwanTinen <74653847+ErwanTinen@users.noreply.github.com>
2023-02-04 18:26:20 -0400

    Thank God, car1 is mooving, but the mouvement is clunky as hell

--- app/src/main/java/com/example/tp1inf8405/GameTest.java
+++ app/src/main/java/com/example/tp1inf8405/GameTest.java
@@ -1,5 +1,6 @@
 package com.example.tp1inf8405;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.Resources;
@@ -10,16 +11,82 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import androidx.gridlayout.widget.GridLayout;;
+import androidx.gridlayout.widget.GridLayout;
 
 public class GameTest extends AppCompatActivity {
     float xDown = 0, yDown = 0;
+
+    public class Bloc{
+        String name;
+        int row;
+        int col;
+        int row_span;
+        int column_span;
+        View view;
+        int type; //0 if vertical, 1 if horizontal
+        Bloc (int row, int col, int row_span, int col_span, View v, String name){
+            this.row = row;
+            this.col = col;
+            this.row_span = row_span;
+            this.column_span = col_span;
+            this.view = v;
+            this.name = name;
+            if (row > col) {
+                this.type = 0;
+            }
+            else{
+                this.type = 1;
+            }
+        }
+
+        @NonNull
+        @Override
+        public String toString() {
+            String res = "for car " + this.name + " /n ";
+            res = res + "row = " + Integer.toString(row) + " /n ";
+            res += "row_span = " + Integer.toString(row_span) + " /n ";
+            res += "col = " + Integer.toString(col) + " /n ";
+            res += "column_span = " + Integer.toString(column_span) + " /n ";
+
+            return res;
+            //return super.toString();
+        }
+
+        public void incCol(){
+            if (((this.col + 1) > 0) & ((this.col + 1) < 8 )){
+                this.col = this.col +1;
+            }
+        }
+        public void decCol(){
+            if (((this.col - 1) > 0) & ((this.col - 1) < 8 )){
+                this.col = this.col -1;
+            }
+        }
+
+        public void incRow(){
+            if ((this.row + 1) < 6 ){
+                this.row = this.row +1;
+            }
+        }
+        public void decRow(){
+            if ((this.row - 1) > -1 ){
+            this.row = this.row -1;}
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_test);
         ImageView bloc = findViewById(R.id.bloc);
+
         View car1 = findViewById(R.id.car1);
+        Bloc car1_bloc = new Bloc(0,1,1,3, car1, "car1");
+
+        View car2 = findViewById(R.id.car2);
+        Bloc car2_bloc = new Bloc(1,3,3,1, car2,"car2");
+
+
+
         bloc.setOnTouchListener(new View.OnTouchListener(){
 
 
@@ -48,7 +115,7 @@ public class GameTest extends AppCompatActivity {
                 return true;
             }
         });
-        car1.setOnTouchListener(new View.OnTouchListener(){
+        car1_bloc.view.setOnTouchListener(new View.OnTouchListener(){
 
 
             public boolean onTouch(View v, MotionEvent event){
@@ -77,12 +144,18 @@ public class GameTest extends AppCompatActivity {
                         Log.d("PARAM",v.getLayoutParams().toString());
                         Log.d("column",layoutParams.columnSpec.toString());
                         if (dpDeltaX > 50) {
-                            //GridLayout.Spec columnSpec = layoutParams.columnSpec;
-                            //int columnSpan = columnSpec.column;
+                            car1_bloc.incCol();
+                        } else if (dpDeltaX < -50) {
+                            car1_bloc.decCol();
                         } else if (dpDeltaY < -50) {
-                            //layoutParams.rowSpec = GridLayout.spec(layoutParams.rowSpec.getGridIndex() + 1);
+                            car1_bloc.decRow();
+                        } else if (dpDeltaY > 50) {
+                            car1_bloc.incRow();
                         }
-                        //v.setLayoutParams(layoutParams);
+                        layoutParams.columnSpec = GridLayout.spec(car1_bloc.col,car1_bloc.column_span);
+                        layoutParams.rowSpec =  GridLayout.spec(car1_bloc.row,car1_bloc.row_span);
+                        Log.d("Result",car1_bloc.toString());
+                        car1_bloc.view.setLayoutParams(layoutParams);
                         break;
 
 
