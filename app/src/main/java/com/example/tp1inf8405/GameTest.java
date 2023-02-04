91214c743a151da36a8962457b23d020df3966fa
ErwanTinen <74653847+ErwanTinen@users.noreply.github.com>
2023-02-04 16:22:40 -0400

    Still testing

--- app/src/main/java/com/example/tp1inf8405/GameTest.java
+++ app/src/main/java/com/example/tp1inf8405/GameTest.java
@@ -2,10 +2,14 @@ package com.example.tp1inf8405;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.GridLayout;
 
 public class GameTest extends AppCompatActivity {
     float xDown = 0, yDown = 0;
@@ -14,6 +18,7 @@ public class GameTest extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_test);
         ImageView bloc = findViewById(R.id.bloc);
+        View car1 = findViewById(R.id.car1);
         bloc.setOnTouchListener(new View.OnTouchListener(){
 
 
@@ -42,5 +47,44 @@ public class GameTest extends AppCompatActivity {
                 return true;
             }
         });
+        car1.setOnTouchListener(new View.OnTouchListener(){
+
+
+            public boolean onTouch(View v, MotionEvent event){
+
+                switch (event.getActionMasked()) {
+                    case MotionEvent.ACTION_DOWN:
+                        xDown = event.getX();
+                        yDown = event.getY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        float movedX, movedY;
+                        movedX = event.getX();
+                        movedY = event.getY();
+                        // calculate how much the user moviedhis finger
+                        float distanceX = movedX - xDown;
+                        float distanceY = movedY - yDown;
+
+                        //now updated position
+                        Resources resources = v.getContext().getResources();
+                        float dpDeltaX = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, distanceX, resources.getDisplayMetrics());
+                        float dpDeltaY = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, distanceY, resources.getDisplayMetrics());
+                        GridLayout.LayoutParams layoutParams = (GridLayout.LayoutParams) v.getLayoutParams();
+
+                        if (dpDeltaX > 50) {
+                            //layoutParams.columnSpec = layoutParams.columnSpec + 1;
+                        } else if (dpDeltaY < -50) {
+                            //layoutParams.rowSpec = GridLayout.spec(layoutParams.rowSpec.getGridIndex() + 1);
+                        }
+                        v.setLayoutParams(layoutParams);
+                        break;
+
+
+
+                }
+                return true;
+            }
+        });
+
     }
 }
