36017a48d29046134789216abd1cc5608b8fe251
ErwanTinen <74653847+ErwanTinen@users.noreply.github.com>
2023-02-05 22:18:57 -0400

    Bon, techniquement ça marche factuellement c'est cassé

--- app/src/main/java/com/example/tp1inf8405/GameTest.java
+++ app/src/main/java/com/example/tp1inf8405/GameTest.java
@@ -9,11 +9,14 @@ import android.util.Log;
 import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.gridlayout.widget.GridLayout;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 
 public class GameTest extends AppCompatActivity {
     float xDown = 0, yDown = 0;
@@ -26,14 +29,33 @@ public class GameTest extends AppCompatActivity {
         int column_span;
         View view;
         boolean isHorizontal; //0 if vertical, 1 if horizontal
-        Bloc (int row, int col, int row_span, int col_span, View v, String name){
+
+        boolean isTarget;
+        Bloc (int row, int col, int row_span, int col_span, View v, String name, boolean isTarget){
             this.row = row;
             this.col = col;
             this.row_span = row_span;
             this.column_span = col_span;
             this.view = v;
             this.name = name;
+            this.isTarget = isTarget;
             if (row_span > col_span) {
+                this.isHorizontal = false;
+            }
+            else{
+                this.isHorizontal = true;
+            }
+        }
+        //Constructeur pour la deepcopy
+        Bloc(Bloc bloc){
+            this.row = bloc.row;
+            this.col = bloc.col;
+            this.row_span = bloc.row_span;
+            this.column_span = bloc.column_span;
+            this.view = bloc.view;
+            this.name = bloc.name;
+            this.isTarget = bloc.isTarget;
+            if (bloc.row_span > bloc.column_span) {
                 this.isHorizontal = false;
             }
             else{
@@ -89,11 +111,42 @@ public class GameTest extends AppCompatActivity {
         int height;
         int[][] grid;
 
+        boolean game_started = false;
+        boolean reseting = false ;
+        ArrayList<Grid> states ; // list to hold the states of the game HEY we could track this if we want to know the number of moves
+        ArrayList<Integer> win_zone ;
         public Grid(int width, int height) {
             blocs = new ArrayList<>();
+            states = new ArrayList<>();
+            win_zone = new ArrayList<>();
             this.width=width;
             this.height = height;
             this.grid = new int[width][height];
+            win_zone.add(2) ;
+            win_zone.add(6); //la zone de victoire est ligne 2 colonne 6
+        }
+        public Grid(Grid grid){
+            //Faut une deepcopy de la Array list... bon
+            this.blocs = new ArrayList<>();
+            for (int k=0; k<grid.blocs.size(); k++){
+                this.blocs.add(new Bloc(grid.blocs.get(k)));
+                Log.d("STATE_CREATION",grid.blocs.get(k).toString());
+            }
+            ; // I hope this is a deepcopy orr I might have prroblems
+            this.width = grid.width;
+            this.height = grid.height;
+            this.grid = new int[width][height];
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j < height; j++) {
+                    this.grid[i][j] = grid.grid[i][j];
+                }
+            }
+            this.win_zone = new ArrayList<>(grid.win_zone);
+        }
+
+        public void game_ready(){
+            game_started = true;
+            states.add(new Grid(this)); // adding the initial state
         }
 
         public void addBloc(Bloc b) {
@@ -102,6 +155,9 @@ public class GameTest extends AppCompatActivity {
                 for (int j = b.col; j < b.col + b.column_span; j++) {
                     grid[i][j] = 1;
                     //Log.d("grid",grid.toString());
+                    if ((game_started) && (!reseting)){
+                        states.add(new Grid(this)); //used to add another state of the grid to the list
+                    }
                 }
             }
         }
@@ -157,6 +213,30 @@ public class GameTest extends AppCompatActivity {
 
             return true;
         }
+
+        public boolean checkWin(Bloc b){
+            if (b.isTarget) {
+                if ((b.col + b.column_span - 1) == (this.width - 1)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void reload(ArrayList<Bloc> b_list){
+            //now we must replace our bloc list with this one and carefully remove and readd every bloc...
+            this.reseting = true;
+            int nb_bloc = b_list.size()-1;
+            for(int k=0; k< nb_bloc; k++){
+                Log.d("LOGGING_blocs",Integer.toString(k));
+                this.removeBloc(this.blocs.get(0)); // If you use k on a list that dynamically change size you will break everything
+            }
+            for(int k=0; k< nb_bloc; k++){
+                this.addBloc(b_list.get(k)); // Look, we add it to the colection AND to the grid, this might work
+            }
+            this.reseting = false;
+        }
+
         @NonNull
         @Override
         public String toString(){
@@ -171,6 +251,47 @@ public class GameTest extends AppCompatActivity {
         }
 
     }
+    /*This function loads the initial state of a Game using what is stored in the Grid */
+    protected void loadInitialState(Grid grid){
+        //remember to like, empty the grid.states an truly reinitialize
+
+        //getting the desired state
+        Grid initialState = grid.states.get(0);
+        ArrayList<Bloc> new_blocs = new ArrayList<>() ; // we will add this to the final grid and then litterally redraw everything as it is added
+        //actually putting blocs back in there place we will use the fact that they are in the same order
+        Log.d("Initial state",initialState.toString());
+        int nb_bloc = grid.blocs.size();
+        for(int k=0 ;k< nb_bloc; k++){
+            //I mean, might as well load the entire initial state right ?
+            Log.d("LOGGING_blocs",Integer.toString(k));
+            Bloc b = grid.blocs.get(k);
+            Log.d("Ancien_bloc",b.toString());
+            Bloc init_b = initialState.blocs.get(k);
+            Log.d("init_bloc",init_b.toString());
+            b.col =  init_b.col;
+            b.row = init_b.row;
+            new_blocs.add(b);
+            Log.d("Nouveau_blocs",b.toString());
+        }
+        //Here you call the function we will create. Its grid.reload.
+        grid.reload(new_blocs);
+        //Now that the virtual grid is reloaded, you need to update the ACTUAL view, goodluck with that my G
+        //Maybe begin by browsing the blocs within ? They should be linked to the same views soits just a matter of updating the views within the blocs... I think
+        for(int k=0 ;k< nb_bloc; k++){
+            //I mean, might as well load the entire initial state right ?
+            Bloc b = grid.blocs.get(k);
+            View v = b.view;
+            GridLayout.LayoutParams layoutParams = (GridLayout.LayoutParams) v.getLayoutParams();
+            //okay now modify the view thingy with the actual parameters of your bloc
+            layoutParams.columnSpec = GridLayout.spec(b.col,b.column_span);
+            layoutParams.rowSpec =  GridLayout.spec(b.row,b.row_span);
+            b.view.setLayoutParams(layoutParams);
+            //AYO THIS MIGHT DO IT
+        }
+
+
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -178,28 +299,28 @@ public class GameTest extends AppCompatActivity {
         ImageView bloc = findViewById(R.id.bloc);
 
         View target = findViewById(R.id.target);
-        Bloc target_bloc = new Bloc(2,1,1,2, target, "car1");
+        Bloc target_bloc = new Bloc(2,1,1,2, target, "target",true);
 
         View car1 = findViewById(R.id.car1);
-        Bloc car1_bloc = new Bloc(0,1,1,3, car1, "car1");
+        Bloc car1_bloc = new Bloc(0,1,1,3, car1, "car1",false);
 
         View car2 = findViewById(R.id.car2);
-        Bloc car2_bloc = new Bloc(1,3,3,1, car2,"car2");
+        Bloc car2_bloc = new Bloc(1,3,3,1, car2,"car2",false);
 
         View car3 = findViewById(R.id.car3);
-        Bloc car3_bloc = new Bloc(3,1,2,1, car3,"car3");
+        Bloc car3_bloc = new Bloc(3,1,2,1, car3,"car3",false);
 
         View car4 = findViewById(R.id.car4);
-        Bloc car4_bloc = new Bloc(5,1,1,3, car4,"car4");
+        Bloc car4_bloc = new Bloc(5,1,1,3, car4,"car4",false);
 
         View car5 = findViewById(R.id.car5);
-        Bloc car5_bloc = new Bloc(4,5,2,1, car5,"car5");
+        Bloc car5_bloc = new Bloc(4,5,2,1, car5,"car5",false);
 
         View car6 = findViewById(R.id.car6);
-        Bloc car6_bloc = new Bloc(3,5,1,2, car6,"car6");
+        Bloc car6_bloc = new Bloc(3,5,1,2, car6,"car6",false);
 
         View car7 = findViewById(R.id.car7);
-        Bloc car7_bloc = new Bloc(0,6,3,1,car7,"car7");
+        Bloc car7_bloc = new Bloc(0,6,3,1,car7,"car7",false);
 
         Grid game = new Grid(7,7);
         game.addBloc(target_bloc);
@@ -210,10 +331,22 @@ public class GameTest extends AppCompatActivity {
         game.addBloc(car5_bloc);
         game.addBloc(car6_bloc);
         game.addBloc(car7_bloc);
+        game.game_ready();
         Log.d("grid",game.toString());
         Log.d("car1",car1_bloc.toString());
         Log.d("car7",car7_bloc.toString());
         ArrayList<Bloc> aList = game.blocs;
+
+        Button res_button = (Button) findViewById(R.id.reset);
+        res_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("RESET_START","you are trying to reset");
+                loadInitialState(game);
+                Log.d("RESET_END","you reseted I hope");
+                // Do something in response to button click
+            }
+        });
+
 
         for (Bloc x : aList) {
             x.view.setOnTouchListener(new View.OnTouchListener() {
@@ -263,9 +396,9 @@ public class GameTest extends AppCompatActivity {
                             if (dpDeltaY < -100) {
                                 Log.d("UP","mooving up");
                                 if ((game.canMoveUp(x)) && (!x.isHorizontal)){
-                                        game.removeBloc(x);
-                                        x.decRow();
-                                        game.addBloc(x);
+                                    game.removeBloc(x);
+                                    x.decRow();
+                                    game.addBloc(x);
                                     Log.d("UP","nice you moved Up");
                                         wasmoved = true;
                                 }
@@ -293,6 +426,14 @@ public class GameTest extends AppCompatActivity {
                                 wasmoved = false;
                             }
                             //car1_bloc.view.setLayoutParams(layoutParams);
+                            break;
+
+                        case MotionEvent.ACTION_UP:
+                            //Update le state uniquement quand on lache la pièce ! Faut le faire ici
+
+                            if (game.checkWin(x)){
+                                //Do a pop up notification or something
+                            }
                             break;
 
                     }
